2c57df73973523fbbfa827f055e124400d32db04
vikas_ver <vikas.ver@ascendcorp.com>
2019-12-04 14:48:42 +0700

    code cleanup

--- src/main/scala/org/WebServer.scala
+++ src/main/scala/org/WebServer.scala
@@ -7,47 +7,38 @@ import akka.stream.ActorMaterializer
 import scala.io.StdIn
 import akka.http.scaladsl.server.Route
 
+object paths {
+  val pathHello1 = path("hello") {
+    concat(path("hello3") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "hello"))
+      }
+    })
+  }
+}
+
 object WebServer {
   def main(args: Array[String]) {
 
     implicit val system = ActorSystem("my-system")
     implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
 
     val route: Route =
-      concat(
-        path("hello") {
-          concat(path("hello3") {
+      pathPrefix("api") {
+        concat(
+          path("info") {
             get {
-              complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "hello"))
+              complete(HttpEntity("this is /api/info path"))
             }
-          })
-        },
-        path("hello1") {
-          get {
-            complete(HttpEntity("Hello1"))
+          },
+          path("details") {
+            get {
+              complete(HttpEntity("this is /api/details path"))
+            }
           }
-        },
-        pathPrefix("hello1") {
-          concat(path("hello12") {
-            get {
-              complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "hello"))
-            }
-          }, path("hello21") {
-            get {
-              complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "hello21"))
-            }
-          })
-        },
-        path("hello2") {
-          get {
-            complete(
-              HttpEntity(ContentTypes.`text/html(UTF-8)`, "Hello! again")
-            )
-          }
-        }
-      )
+        )
+      }
 
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
